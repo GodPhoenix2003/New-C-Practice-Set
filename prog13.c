8c8059f1e56051abb449ecf09894a1994331732c
Sagnik Chatterjee <100948753+GodPhoenix2003@users.noreply.github.com>
2022-07-27 22:46:18 +0530

    Update prog13.c

--- prog13.c
+++ prog13.c
@@ -6,4 +6,8 @@ void main(){
     printf("\nEnter the string: ");
     gets(x);
     printf("\nLength of the string '%s' is: %d\n", x, strlen(x));
+    /*int size = 0;
+    for(int i = 0; x[i] != '\0'; i++)
+        size++;
+    printf("\nSize = %d\n", size);*/
 }
